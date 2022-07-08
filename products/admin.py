dc917a2e742955157669afc35892a522693bdee7
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-09 00:55:24 +0100

    Update

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Product
 
 
-# Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Product, ProductAdmin)
